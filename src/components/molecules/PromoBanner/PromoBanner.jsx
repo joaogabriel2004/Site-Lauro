895a2cb7703a23ac0a57c8395a93c70671c6261b
João Gabriel <joaogabrielsj2004@gmail.com>
2024-06-11 22:20:47 -0300

    att

--- src/components/molecules/PromoBanner/PromoBanner.jsx
+++ src/components/molecules/PromoBanner/PromoBanner.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Image from '../../atoms/Image/Image';
 import Text from '../../atoms/Text/Text';
+import Laptop from '../../assets/Lap top.png'
+import Pessoa from '../../assets/Cara usando Lap top.png'
 import './PromoBanner.css';
 
 const PromoBanner = () => {
     return (
         <div className="promo-banner">
-            <Image alt="Pessoa com laptop" />
+            <Image alt="Pessoa com laptop" src={Pessoa}/>
             <Text>Laptops à partir de 90% de desconto</Text>
-            <Image alt="Laptop" />
+            <Image alt="Laptop" src={Laptop} className="Laptop"/>
         </div>
     );
 };
